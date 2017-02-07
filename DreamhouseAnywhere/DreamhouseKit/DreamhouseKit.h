d3e0171abb24af1a75c7f043411cdbd91992ec78
quintonwall <quinton_wall@hotmail.com>
2017-02-07 10:33:08 -0800

    updated graphics

---
+++ DreamhouseAnywhere/DreamhouseKit/DreamhouseKit.h
@@ -0,0 +1,19 @@
+//
+//  DreamhouseKit.h
+//  DreamhouseKit
+//
+//  Created by QUINTON WALL on 2/3/17.
+//  Copyright © 2017 me.quinton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DreamhouseKit.
+FOUNDATION_EXPORT double DreamhouseKitVersionNumber;
+
+//! Project version string for DreamhouseKit.
+FOUNDATION_EXPORT const unsigned char DreamhouseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DreamhouseKit/PublicHeader.h>
+
+
