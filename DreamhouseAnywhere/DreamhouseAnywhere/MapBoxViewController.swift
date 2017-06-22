2464bd82ad0c1df988b28ff2308052e769bb671a
quintonwall <quinton_wall@hotmail.com>
2017-06-21 18:59:21 -0700

    messing with custom callout views on map box

--- DreamhouseAnywhere/DreamhouseAnywhere/MapBoxViewController.swift
+++ DreamhouseAnywhere/DreamhouseAnywhere/MapBoxViewController.swift
@@ -36,13 +36,14 @@ class MapBoxViewController: UIViewController, MGLMapViewDelegate {
             let point = MGLPointAnnotation()
             point.coordinate = CLLocationCoordinate2D(latitude: p.latitude, longitude: p.longitude)
             point.title = p.title
-            point.subtitle = p.description
+            point.subtitle = p.price.currencyString()
+            
             
             mapView.addAnnotation(point)
         }
     }
     
-    
+    // MARK: MapBox delegate methods
     func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         // Always try to show a callout when an annotation is tapped.
         return true
@@ -55,6 +56,74 @@ class MapBoxViewController: UIViewController, MGLMapViewDelegate {
     }
     
     /*
+    * for custom popups
+    func mapView(_ mapView: MGLMapView, calloutViewFor annotation: MGLAnnotation) -> MGLCalloutView? {
+        var selectedProperty: Property?
+        for p in properties {
+            if annotation.title! == p.title! {
+                selectedProperty = p
+                print("selected \(p.title!)")
+            }
+        }
+        return MapBoxCustomCalloutView(representedObject: annotation, property: selectedProperty!)
+    }
+ */
+    
+    
+     //make a custom annotation pin t
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        // This example is only concerned with point annotations.
+        guard annotation is MGLPointAnnotation else {
+            return nil
+        }
+        
+        // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
+        let reuseIdentifier = "\(annotation.coordinate.longitude)"
+        
+        // For better performance, always try to reuse existing annotations.
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        
+        // If there’s no reusable annotation view available, initialize a new one.
+        if annotationView == nil {
+            annotationView = PropertyAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView!.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+            
+            annotationView!.backgroundColor = ColorPallete.init().flatGreen
+        }
+        
+        return annotationView
+    }
+  
+    
+    /*
+    //use an image for a custom annoation
+    // comment this func out if you want to use the standard default annotation pins
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        // Try to reuse the existing ‘pisa’ annotation image, if it exists.
+        var annotationImage = mapView.dequeueReusableAnnotationImage(withIdentifier: "homepin")
+        
+        if annotationImage == nil {
+            // Leaning Tower of Pisa by Stefan Spieler from the Noun Project.
+            var image = UIImage(named: "home-pin")!
+            
+            // The anchor point of an annotation is currently always the center. To
+            // shift the anchor point to the bottom of the annotation, the image
+            // asset includes transparent bottom padding equal to the original image
+            // height.
+            //
+            // To make this padding non-interactive, we create another image object
+            // with a custom alignment rect that excludes the padding.
+            image = image.withAlignmentRectInsets(UIEdgeInsets(top: 0, left: 0, bottom: image.size.height/2, right: 0))
+            
+            // Initialize the ‘pisa’ annotation image with the UIImage we just loaded.
+            annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "homepin")
+        }
+        
+        return annotationImage
+    }
+    */
+    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -65,3 +134,31 @@ class MapBoxViewController: UIViewController, MGLMapViewDelegate {
     */
 
 }
+
+class PropertyAnnotationView: MGLAnnotationView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Force the annotation view to maintain a constant size when the map is tilted.
+        scalesWithViewingDistance = false
+        
+        // Use CALayer’s corner radius to turn this view into a circle.
+        layer.cornerRadius = frame.width / 2
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Animate the border width in/out, creating an iris effect.
+        
+        let animation = CABasicAnimation(keyPath: "borderWidth")
+        animation.duration = 0.1
+        layer.borderWidth = selected ? frame.width / 4 : 2
+        layer.add(animation, forKey: "borderWidth")
+        
+    }
+}
+
+
